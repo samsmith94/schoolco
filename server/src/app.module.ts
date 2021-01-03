b32bd01921cdb338a5962fd0bc918f2a93327ea3
Sámuel Kovács <sunikovacs@gmail.com>
2021-01-03 16:10:03 +0100

    feat: add google authorization & jwt authentication

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,11 +6,13 @@ import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import * as Joi from '@hapi/joi';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
-    UsersModule
+    UsersModule,
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ server/src/auth/google.strategy.ts
@@ -0,0 +1,41 @@
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { Strategy } from "passport-google-oauth20";
+
+import { AuthService, Provider } from "./auth.service";
+
+import { config } from 'dotenv';
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google')
+{
+
+    constructor(private readonly authService: AuthService) {
+        super({
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_SECRET,
+            callbackURL: 'http://localhost:3000/auth/google/callback',
+            passReqToCallback: true,
+            scope: ['profile']
+        })
+    }
+
+    async validate(request: any, accessToken: string, refreshToken: string, profile, done: Function) {
+        try {
+            console.log(profile);
+
+            const jwt: string = await this.authService.validateOAuthLogin(profile.id, Provider.GOOGLE);
+            const user =
+            {
+                jwt
+            }
+
+            done(null, user);
+        }
+        catch (err) {
+            // console.log(err)
+            done(err, false);
+        }
+    }
+
+}
